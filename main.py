dada5456da907f4a6676d1813f2050945bb417f5
Muhammad Akand <akandmuhammad@tuta.io>
2024-03-20 12:44:33 +1100

    Initial commit

---
+++ main.py
@@ -0,0 +1,56 @@
+from tkinter import *
+from tkinter import filedialog
+from pygame import mixer
+import os
+
+# App Initialization
+app = Tk()
+app.title('Music Player')
+app.geometry('485x700')
+app.config(bg='#2A2D34')
+app.resizable(False, False)
+mixer.init()
+
+musicFrame = Frame(app, bd=2, relief=RIDGE)
+musicFrame.place(x=0, y=530, width=485, height=150)
+
+lowerFrame = Frame(app, bg='#30C5FF', width=485, height=100)
+lowerFrame.place(x=0, y=400)
+
+
+# Play Music
+def playMusic():
+    musicName = Playlist.get(ACTIVE)
+    print(musicName)
+    mixer.music.load(musicName)
+    mixer.music.play()
+
+
+# Buttons
+Button(app, text='Play', height=6, width=12, command=playMusic).place(x=200, y=400)
+Button(app, text='Unpause', height=6, width=12, command=mixer.music.unpause).place(x=50, y=400)
+Button(app, text='Pause', height=6, width=12, command=mixer.music.pause).place(x=340, y=400)
+
+
+# Browse files for music
+def addMusic():
+    path = filedialog.askdirectory()
+    if path:
+        os.chdir(path)
+        songs = os.listdir(path)
+
+        for song in songs:
+            if song.endswith('.mp3'):
+                Playlist.insert(END, song)
+
+
+# Button and Music Box
+Button(app, text='Browse Music', width=68, height=1, fg='#30C5FF', bg='#2A2D34', command=addMusic).place(x=0, y=500)
+Scroll = Scrollbar(musicFrame)
+Playlist = Listbox(musicFrame, width=100, bg="#333333", fg="grey", selectbackground="lightblue", cursor="hand2", bd=0,
+                   yscrollcommand=Scroll.set)
+Scroll.config(command=Playlist.yview)
+Scroll.pack(side=RIGHT, fill=Y)
+Playlist.pack(side=RIGHT, fill=BOTH)
+
+app.mainloop()
